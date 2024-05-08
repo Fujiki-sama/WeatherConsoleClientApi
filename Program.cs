259678d8cc12bcdbe72a0ca780707ce75de698c2
Shago Tcholadze <113519867+Fujiki-sama@users.noreply.github.com>
2024-05-08 12:48:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// My API Key: fa3871e648f64a0a978a7bc78d9c35c1
+
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace WeatherConsoleClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            Console.Write("Welcome to the Open Weather API!\nPlease input your API Key: ");
+            var apiKey = Console.ReadLine();
+
+            Console.Write("\nThank you.\n\nEnter the city: ");
+            var cityName = Console.ReadLine().ToLower();
+
+            var userURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric";
+
+            var weatherResponse = client.GetStringAsync(userURL).Result;
+
+            dynamic formattedResponse = JsonConvert.DeserializeObject(weatherResponse);
+
+            var temp = formattedResponse.main.temp;
+
+            Console.WriteLine($"\n{temp} degrees Celsius.");
+        }
+    }
+}
